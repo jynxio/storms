95856f6db145889149f195e962d94c54c61c888e
X <1337816495@qq.com>
2022-02-14 16:45:12 +0800

    u

--- source/index.js
+++ source/index.js
@@ -4,7 +4,13 @@ import "/style/index.css";
 
 import * as three from "three";
 
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 import { toMercator } from "@turf/projection";
+
+import earcut from "earcut";
+
+import turfunion from "@turf/union";
 
 /* ------------------------------------------------------------------------------------------------------ */
 /* Renderer */
@@ -23,20 +29,43 @@ const camera = new three.OrthographicCamera();
 
 scene.add( camera );
 
+/* Controls */
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.enableDamping = true;
+controls.enableRotate = false;
+
 /* Render */
 renderer.setAnimationLoop( function loop() {
+
+    controls.update();
 
     renderer.render( scene, camera );
 
 } );
 
 /* ------------------------------------------------------------------------------------------------------ */
+main();
 
-getData();
+async function main() {
+
+    let data;
+
+    data = await getData();
+
+    updateFrustum( data );
+
+    data = union( data );
+
+    drawMesh( data );
+
+}
 
 async function getData() {
 
-    /*  */
+    let output;
+
+    /* File name */
     const FILE_NAMES = [
         "2020_ARTHUR_2020138N28281",
         "2020_BERTHA_2020148N32281",
@@ -61,9 +90,7 @@ async function getData() {
     ];
 
     /* Load data */
-    let data;
-
-    data = await Promise.all( FILE_NAMES.map( file_name => load( "/static/" + file_name + ".json" ) ) );
+    output = await Promise.all( FILE_NAMES.map( file_name => load( "/static/" + file_name + ".json" ) ) );
 
     async function load( url ) {
 
@@ -77,9 +104,9 @@ async function getData() {
     }
 
     /* Clean data */
-    data = clean( data );
+    output = clean( output );
 
-    function ( input ) {
+    function clean( input ) {
 
         const output = [];
 
@@ -89,71 +116,20 @@ async function getData() {
 
             features.forEach( item => {
 
-                const center_4326 = item.geometry.coordinates;
-
-                // TODO
-
-            } );
-
-        } );
-
-    }
-
-    function clean( input ) {
-
-        const output = [];
-
-        input.forEach( item => { // 数据粒度：json
-
-            const features = item.data.storm.features;
-
-            features.forEach( item => { // 数据粒度：GeoJSON Object
-
-                const center_4326 = item.geometry.coordinates;
-                const center_4326_geojson_object = {
+                const point_4326 = {
                     type: "Feature",
                     geometry: {
                         type: "Point",
-                        coordinates: center_4326,
+                        coordinates: item.geometry.coordinates,
                     },
-                    properties: {},
-                };
-                const center_3857_geojson_object = toMercator( center_4326_geojson_object );
-
-                /* 根据storm由Point数据解构为Polygon数据。 */
-                const polygon_geojson_object = {
-                    type: "Feature",
-                    geometry: {
-                        type: "Polygon",
-                        coordinates: [ [] ], // 逆时针、linear ring
+                    properties: {
+                        radius: item.properties.windSpeed * 5000,
                     },
-                    properties: {},
                 };
 
-                output.push( polygon_geojson_object );
+                const point_3857 = toMercator( point_4326 );
 
-                const dock = polygon_geojson_object.geometry.coordinates[ 0 ];
-                const center_3857 = center_3857_geojson_object.geometry.coordinates;
-                const radius = item.properties.windSpeed * 5000;
-
-                let segments = 64; // 该参数控制圆形的弧段数。
-                const delta = Math.PI * 2 / segments;
-
-                dock.push( [
-                    Math.cos( delta * 0 ) * radius + center_3857[ 0 ],
-                    Math.sin( delta * 0 ) * radius + center_3857[ 1 ],
-                ] );
-
-                while( --segments >= 0 ) {
-
-                    let theta = delta * segments;
-
-                    dock.push( [
-                        Math.cos( theta ) * radius + center_3857[ 0 ],
-                        Math.sin( theta ) * radius + center_3857[ 1 ],
-                    ] );
-
-                }
+                output.push( point_3857 );
 
             } );
 
@@ -162,5 +138,236 @@ async function getData() {
         return output;
 
     }
+
+    /* Deconstruct data */
+    output = deconstruct( output );
+
+    function deconstruct( input ) {
+
+        const output = input.map( point => {
+
+            const polygon = {
+                type: "Feature",
+                geometry: {
+                    type: "Polygon",
+                    coordinates: [
+                        [], // linear ring，因为它用于表示图形的轮廓，而非孔，因此它以逆时针的顺序来存储position。
+                    ],
+                },
+                properties: {},
+            };
+
+            let segments = 64; // 圆形的弧段数。
+            const delta = Math.PI * 2 / segments;
+
+            const center = point.geometry.coordinates;
+            const radius = point.properties.radius;
+
+            const linear_ring = polygon.geometry.coordinates[ 0 ];
+
+            linear_ring.push( [
+                Math.cos( delta * 0 ) * radius + center[ 0 ],
+                Math.sin( delta * 0 ) * radius + center[ 1 ],
+            ] );
+
+            while ( --segments >= 0 ) {
+
+                let theta = delta * segments;
+
+                linear_ring.push( [
+                    Math.cos( theta ) * radius + center[ 0 ],
+                    Math.sin( theta ) * radius + center[ 1 ],
+                ] );
+
+            }
+
+            return polygon;
+
+        } );
+
+        return output;
+
+    }
+
+    return output;
+
+}
+
+function updateFrustum( input ) {
+
+    /* 计算数据的极值。 */
+    const xs = [];
+    const ys = [];
+
+    input.forEach( polygon => {
+
+        const positions = polygon.geometry.coordinates[ 0 ];
+
+        positions.forEach( position => {
+
+            const [ x, y ] = position;
+
+            xs.push( x );
+            ys.push( y );
+
+        } );
+
+    } );
+
+    /* 更新相机的视野范围。 */
+    const boundary = {};
+    const viewport = {};
+
+    main();
+
+    window.addEventListener( "resize", _ => {
+
+        main();
+
+        renderer.setPixelRatio( Math.min( devicePixelRatio ) );
+        renderer.setSize( viewport.width, viewport.height );
+
+    } );
+
+    function main() {
+
+        boundary.left = Math.min( ...xs );
+        boundary.right = Math.max( ...xs );
+        boundary.top = Math.max( ...ys );
+        boundary.bottom = Math.min( ...ys );
+        boundary.width = ( boundary.right - boundary.left ) * 1.05;
+        boundary.height = ( boundary.top - boundary.bottom ) * 1.05;
+        boundary.aspect = boundary.width / boundary.height;
+
+        viewport.width = innerWidth;
+        viewport.height = innerHeight;
+        viewport.aspect = viewport.width / viewport.height;
+
+        const aspect_ratio = boundary.aspect / viewport.aspect;
+
+        aspect_ratio > 1
+        ? boundary.height *= aspect_ratio
+        : boundary.width  /= aspect_ratio;
+
+        camera.left = - boundary.width / 2;
+        camera.right = boundary.width / 2;
+        camera.top = boundary.height / 2;
+        camera.bottom = - boundary.height / 2;
+        camera.near = 0.1;
+        camera.far = 2;
+        camera.updateProjectionMatrix();
+        camera.position.x = ( boundary.left + boundary.right ) / 2;
+        camera.position.y = ( boundary.bottom + boundary.top ) / 2;
+        camera.position.z = 1;
+
+        controls.target.x = camera.position.x;
+        controls.target.y = camera.position.y;
+        controls.target.z = 0;
+        controls.update();
+
+    }
+
+}
+
+function drawMesh( input ) {
+
+    if ( input.geometry.type !== "MultiPolygon" ) {
+
+        console.error( "该函数只处理MultiPolygonn类型的GeoJSON。" );
+
+        return;
+
+    }
+
+    input.geometry.coordinates.forEach( polygon_coordinates => {
+
+        const color = Math.round( Math.random() * 0xffffff );
+        const wireframe = false;
+
+        scene.add( createMesh( polygon_coordinates, color, wireframe ) );
+
+    } );
+
+    input.geometry.coordinates.forEach( polygon_coordinates => {
+
+        scene.add( createMesh( polygon_coordinates, 0xffffff, true ) );
+
+    } );
+
+    function createMesh( polygon_coordinates, color, wireframe ) {
+
+        /* flatten数据 */
+        const coordinates = polygon_coordinates;
+
+        const vertex = [];
+        const hole = [];
+        const dimensions = 3;
+
+        for ( let i = 0; i < coordinates.length; i++ ) {
+
+            const linear_ring = coordinates[ i ];
+
+            /* 处理轮廓数据。 */
+            linear_ring.forEach( position => {
+
+                const [ x, y ] = position;
+
+                vertex.push( x, y, 0 );
+
+            } );
+
+            if ( i <= 0 ) continue;
+
+            /* 处理孔数据。 */
+            const previous_linear_ring = coordinates[ i - 1 ];
+
+            hole.push(
+                hole.length === 0
+                ? previous_linear_ring.length
+                : previous_linear_ring.length + hole[ hole.length - 1 ]
+            );
+
+        }
+
+        /* 生成mesh */
+        const geometry = new three.BufferGeometry();
+        const material = new three.MeshBasicMaterial( { side: three.DoubleSide, color, wireframe } );
+        const mesh = new three.Mesh( geometry, material );
+
+        let position;
+        position = new Float32Array( vertex );
+        position = new three.BufferAttribute( position, 3 );
+
+        geometry.setAttribute( "position", position );
+
+        let index;
+        index = new Uint16Array( earcut( vertex, hole.length === 0 ? null : hole, dimensions ) );
+        index = new three.BufferAttribute( index, 1 );
+
+        geometry.setIndex( index );
+        geometry.index.needsUpdate = true;
+
+        geometry.computeBoundingBox();
+        geometry.computeBoundingSphere();
+
+        return mesh;
+
+    }
+
+}
+
+function union( input ) {
+
+    let output = input[ 0 ];
+
+    let index = 0;
+
+    while ( ++index < input.length ) {
+
+        output = turfunion( output, input[ index ] );
+
+    }
+
+    return output;
 
 }
